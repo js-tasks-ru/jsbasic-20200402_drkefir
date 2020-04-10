f116d65c0b7b3c6ffb37cab26d07f175be620982
Jelissej Eirich <jelissej.eirich@gmx.ch>
2020-04-10 16:59:48 +0200

    Определение факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  if (n === 0 || n === 1) {
+    result = 1;
+  } else if (n === 2) {
+    result = 2;
+  } else {
+    for (i = 1; i < n; i++) {
+      step = n - i;
+      result *= step;
+    }
+  }
+  return result;
 }
